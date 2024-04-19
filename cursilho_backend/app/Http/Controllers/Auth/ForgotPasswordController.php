ea75c49abd412a823bbe9dce7dc6f6369a18596b
luanfelipeantunes <140464814+luanfelipeantunes@users.noreply.github.com>
2024-04-19 20:04:46 -0300

    Iniciando sistema de autenticação

---
+++ cursilho_backend/app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class ForgotPasswordController extends Controller
+{
+    public function store(Request $request){
+        $request->validate([
+            'email' => 'required|email',
+        ]);
+
+        $status = Password::sendResetLink(
+            $request->only('email')
+        );
+
+        return $status === Password::RESET_LINK_SENT
+        ? back() -> with('status', __($status))
+        : back()->withInput($request->only('email'))->withErrors(['email' => __($status)]);
+    }
+
+
+    public function reset(Request $request){
+        $request->validate([
+            'token' => 'required',
+            'email' => 'required|email',
+            'password' => 'required | confirmed'
+        ]);
+
+        $status = Password::reset(
+            $request->only('email', 'password', 'password_confirmation', 'token'),
+            function ($user) use ($request) {
+                $user->forceFill([
+                    'password' => Hash::make($request->password),
+                    'remember_token' => Str::random(60)
+                ])->save();
+            }
+        );
+
+        return $status == Password::PASSWORD_RESET
+        ? redirect()->route('login')->with('status', __($status))
+        : back()->withInput($request -> only('email'))->withErrors(['email' => __($status)]);
+    }
+}
--- cursilho_backend/routes/api.php
+++ cursilho_backend/routes/api.php
@@ -3,6 +3,10 @@
 use App\Http\Controllers\EventsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\HttpKernel\DependencyInjection\RegisterControllerArgumentLocatorsPass;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\ForgotPasswordController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,7 +24,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
-//Eventos
+//Events
 Route::group(['prefix' => 'events'], function(){
     Route::post('/', [EventsController::class, 'store']);
     Route::get('/', [EventsController::class, 'index']) -> name('events.index');
@@ -28,4 +32,15 @@ Route::group(['prefix' => 'events'], function(){
     Route::patch('/{id}', [EventsController::class, 'update']);
     Route::get('/{id}', [EventsController::class, 'edit']);
     Route::get('/show/{id}', [EventsController::class, 'show']);
+});
+
+
+//Users
+Route::group(['prefix' => 'users'], function() {
+    Route::get('/register', [RegisterController::class, 'create']);
+    Route::post('/register', [RegisterController::class, 'store']);
+    Route::post('/logout', [LogoutController::class, 'destroy'])
+        ->middleware('auth');
+    Route::post('/forgot-password', [ForgotPasswordController::class, 'store']);
+    Route::post('/forgot-password/{token}', [ForgotPasswordController::class, 'reset']);
 });
